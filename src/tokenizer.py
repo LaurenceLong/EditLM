2aea43518bc60caeaf02e58ded9f48f8c7f9bdcd
Laurence <longzbc@gmail.com>
2025-04-24 19:16:43 +0800

    fix tokenizer.py

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -6,11 +6,12 @@ def get_tokenizer(base, **kwargs):
     tokenizer = AutoTokenizer.from_pretrained(base, **kwargs)
     if tokenizer.pad_token is None:
         tokenizer.pad_token = tokenizer.eos_token
+        tokenizer.pad_token_id = tokenizer.convert_tokens_to_ids(tokenizer.pad_token)
+
     # 获取 <|delete_token_id|>标记的ID
-    del_token_id = tokenizer.convert_tokens_to_ids(tokenizer.pad_token)
-    print(f" <|delete_token_id|>标记的ID为: {del_token_id}")
+    print(f" <|delete_token_id|>标记的ID为: {tokenizer.pad_token_id}")
     return tokenizer
 
 
 def get_del_token_id(tokenizer):
-    return tokenizer.pad_token
+    return tokenizer.pad_token_id
